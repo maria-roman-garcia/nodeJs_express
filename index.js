2d612635ed3619bacb4f95c0c7ac60bb2ea0ce90
Maria Roman garcia <mromangarcia04@gmail.com>
2024-08-01 10:03:28 +0200

    post method to create new product

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ const http = require('http');
 
 const server = http.createServer();
 
+const products = [{ name: 'banana' }, { name: 'apple' }, { name: 'orange' }];
+
 server.on('request', (req, res) => {
     console.log(req.method);
+    console.log(req.url);
 
-    res.setHeader("Content-Type", "text/plain");
+    if(req.url === '/'){
+        res.setHeader("Content-Type", "text/html");
+        res.end(`
+            <form action="/products" method="POST">
+                <input type="text" name="productName"/>
+                <button type="submit">Post</button>
+            </form>
+        `)
+    } else if (req.url === '/products'){
 
-    if(req.method === 'GET'){
-        //res.writeHead(200, { "Content-Type": "text/plain" });
-        res.statusCode = 200;
-        res.end("Hello, this is a NodeJs Http Server - GET Request handled"); //finalize http connection
+        if(req.method === 'POST'){
+            res.end('POST request handled');
+        } else if (req.method === 'GET') {
+            res.setHeader("Content-Type", "application/json");
+    
+            //res.writeHead(200, { "Content-Type": "text/plain" });
+            res.statusCode = 200;
+            res.end(JSON.stringify(products)); //finalize http connection
+        } else {
+            res.setHeader("Content-Type", "text/plain");
+            res.statusCode = 405;
+            res.end("Method Not Allowed"); //finalize http connection
+        }
     } else {
-        res.statusCode = 405;
-        res.end("Method Not Allowed"); //finalize http connection
+        res.setHeader("Content-Type", "text/plain");
+            res.statusCode = 404;
+            res.end("Page Not Found!"); //finalize http connection
     }
 });
 
