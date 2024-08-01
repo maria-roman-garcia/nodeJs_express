4710b03e5452d50807dcb696a3cc96716f1b8857
Maria Roman garcia <mromangarcia04@gmail.com>
2024-08-01 09:41:52 +0200

    server handled methods

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.end("Hello, this is a NodeJs Http Server"); //finalize http connection
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    console.log(req.method);
+
+    res.setHeader("Content-Type", "text/plain");
+
+    if(req.method === 'GET'){
+        //res.writeHead(200, { "Content-Type": "text/plain" });
+        res.statusCode = 200;
+        res.end("Hello, this is a NodeJs Http Server - GET Request handled"); //finalize http connection
+    } else {
+        res.statusCode = 405;
+        res.end("Method Not Allowed"); //finalize http connection
+    }
 });
 
 server.listen(3000, () => {
